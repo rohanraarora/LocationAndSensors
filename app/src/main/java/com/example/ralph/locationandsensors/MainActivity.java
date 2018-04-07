18e69d6f74aaafaf0e3b509377fde21344b1d75b
Rohan <rohanraarora@gmail.com>
2018-04-07 13:36:07 +0530

    Sensors code

--- app/src/main/java/com/example/ralph/locationandsensors/MainActivity.java
+++ app/src/main/java/com/example/ralph/locationandsensors/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.ralph.locationandsensors;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,47 +37,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+//
+//        client = LocationServices.getFusedLocationProviderClient(this);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//
+//                if(location != null){
+//                    textView.setText(location.getLatitude() + " : " + location.getLongitude());
+//                }
+//
+//            }
+//        });
 
-        client = LocationServices.getFusedLocationProviderClient(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+
+        SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = manager.getSensorList(Sensor.TYPE_ALL);
+        Sensor lightSensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(lightSensor != null){
+            manager.registerListener(new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+                    float[] data = event.values;
+                    float lux = data[0];
+                    Log.d("LightSensor","Lux: " + lux);
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            },lightSensor,3000000);
         }
-        client.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
+        else {
+            Toast.makeText(this,"No light sensor",Toast.LENGTH_SHORT).show();
+        }
 
-                if(location != null){
-                    textView.setText(location.getLatitude() + " : " + location.getLongitude());
-                }
-
-            }
-        });
-
-        request = LocationRequest.create();
-        request.setInterval(3000);
-        request.setFastestInterval(2000);
-        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-         callback = new LocationCallback(){
-
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                List<Location> locations = locationResult.getLocations();
-                if(locations != null && locations.size() > 0){
-                    Location location = locations.get(0);
-                    Toast.makeText(MainActivity.this,location.getLatitude() + " : " + location.getLongitude(),Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        };
+//        request = LocationRequest.create();
+//        request.setInterval(3000);
+//        request.setFastestInterval(2000);
+//        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+//
+//         callback = new LocationCallback(){
+//
+//            @Override
+//            public void onLocationResult(LocationResult locationResult) {
+//                super.onLocationResult(locationResult);
+//                List<Location> locations = locationResult.getLocations();
+//                if(locations != null && locations.size() > 0){
+//                    Location location = locations.get(0);
+//                    Toast.makeText(MainActivity.this,location.getLatitude() + " : " + location.getLongitude(),Toast.LENGTH_SHORT).show();
+//                }
+//
+//            }
+//        };
 
 
 
@@ -85,22 +113,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        client.removeLocationUpdates(callback);
+       // client.removeLocationUpdates(callback);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        client.requestLocationUpdates(request,callback,null);
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return;
+//        }
+//        client.requestLocationUpdates(request,callback,null);
     }
 }
